1e99ab0d05647ec232ede8b923527040c8a35769
Yuyan Li <yuyanli1211@gmail.com>
2022-07-28 19:45:12 +1000

    fixed path

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,10 @@ export default function Footer() {
     return (
         <footer className="footer">
             <a href="https://www.linkedin.com/in/yuyan-li-895634232/">
-                <img src="public/Linkedin_Icon.png" alt="linkedin" className="icons" />
+                <img src="assets/Linkedin_Icon.png" alt="linkedin" className="icons" />
             </a>
             <a href="https://github.com/Yuyan-Li1">
-                <img src="public/GitHub_Icon.png" alt="github" className="icons" />
+                <img src="assets/GitHub_Icon.png" alt="github" className="icons" />
             </a>
         </footer>
     )
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import profile_pic from "/assets/profile_pic.png";
 
 export default function Info() {
     return (
         <div className="info">
-            <img src="public/profile_pic.png" alt="profile picture" />
+            <img src={profile_pic} alt="profile picture" />
             <h1 className="name">Yuyan Li</h1>
             <h2 className="info_title">Software Developer</h2>
             <a className="portfolio" href="https://yuyanli.dev">https://yuyanli.dev</a>
             <a className="button" href="mailto:hi@yuyanli.dev">
-                <img src="public/mail_icon.png" alt="email" />
+                <img src="assets/mail_icon.png" alt="email" />
                 <p className="email_text">Email</p>
             </a>
         </div >
